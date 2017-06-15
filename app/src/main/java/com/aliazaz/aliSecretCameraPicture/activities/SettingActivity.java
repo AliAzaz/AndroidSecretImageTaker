de806bc64632ffdac8854e44a6429687e93852cd
ali.azaz <openmind_ali@hotmail.com>
2017-06-15 14:10:54 +0500

    fixed bug of capturing time

--- app/src/main/java/com/aliazaz/aliSecretCameraPicture/activities/SettingActivity.java
+++ app/src/main/java/com/aliazaz/aliSecretCameraPicture/activities/SettingActivity.java
@@ -89,7 +89,7 @@ public class SettingActivity extends AppCompatActivity {
 
         if (Integer.parseInt(numCap.getText().toString()) > Integer.parseInt(capTime.getText().toString())) {
             Toast.makeText(this, "Invalid(data): " + getString(R.string.numImgCaption), Toast.LENGTH_SHORT).show();
-            numCap.setError("Invalid(data): Range greater then capturing time");    // Set Error on last radio button
+            numCap.setError("Invalid(data): Range less then capturing time");    // Set Error on last radio button
             return false;
         } else {
             numCap.setError(null);
